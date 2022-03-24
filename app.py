6a3ac63093376c729af355c64a696a75e3ef0164
ashwinshetgaonkar <75084722+ashwinshetgaonkar@users.noreply.github.com>
2022-03-24 14:55:30 +0530

    added app.py file

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import joblib
+import streamlit as st
+
+skew_cols=[' C', ' Si', ' Mn', ' P', ' S', ' Ni', ' Cr', ' Mo', ' Cu', 'V', ' Al', 'Ceq', 'Nb + Ta']
+
+inputs={}
+
